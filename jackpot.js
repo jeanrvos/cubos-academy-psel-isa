3b46bf0b9b4e2dbd21ae35faa3eb554d82aa4939
jeanrvos <velasco.jean@gmail.com>
2021-03-09 21:27:12 -0300

    inclui minha resposta ao desafio 02

---
+++ jackpot.js
@@ -0,0 +1,10 @@
+// Desafio 02 - Jackpot
+
+function solucao(resultado1, resultado2, resultado3) {
+    if(resultado1 == resultado2 && resultado2 == resultado3) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+    
