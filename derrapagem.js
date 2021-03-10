09a9e599b5305797cc55128f05042ae504751c96
jeanrvos <velasco.jean@gmail.com>
2021-03-09 21:27:23 -0300

    inclui minha resposta ao desafio 03

---
+++ derrapagem.js
@@ -0,0 +1,7 @@
+// Desafio 03 - Derrapagem
+
+function solucao(velocidade, aceleracao) {
+	//seu código aqui  
+    let resultado = ((velocidade * velocidade) * -1) / (2 * aceleracao);
+    console.log(resultado);  
+}
